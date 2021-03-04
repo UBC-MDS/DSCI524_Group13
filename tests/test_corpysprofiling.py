35a7b675f0edc5acb6ac74b8527edc86da0521bb
Elanor Boyle-Stanley <elanor.boyle.stanley@gmail.com>
2021-03-04 09:10:57 -0500

    Write initial tests

--- tests/test_corpysprofiling.py
+++ tests/test_corpysprofiling.py
@@ -3,3 +3,14 @@ from corpysprofiling import corpysprofiling
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_corpora_compare():
+    """ Test corpora_compare function"""
+    corpus1 = "Test1"
+    corpus2 = "Test1"
+
+    # test output type is float
+    assertTrue (is_float(corpysprofiling.corpora_compare(corpus1, corpus2)))
+
+    # test identical corpora return a score of 0.0
+    assert (corpysprofiling.corpora_compare(corpus1, corpus2) == 0.0)
