09f65dae85ef30c61a102ccae3ce25e2f74bcc81
Elanor Boyle-Stanley <elanor.boyle.stanley@gmail.com>
2021-03-06 09:05:24 -0500

    Uncomment tests

--- tests/test_corpysprofiling.py
+++ tests/test_corpysprofiling.py
@@ -13,14 +13,14 @@ def test_corpus_viz():
     corpus1 = "How many species of animals are there in Russia?"
     corpus2 = "Let's assume that this unsupervised model is used to assist human experts to identify fraud transaction. So instead of making humans examine all 284,807 transactions for fraud transactions, this model would extract transactions which look suspicious and pass them to humans for examination. So our goal is to come up with a list of transactions which look suspicious.We said before that PCA won't be able to capture characteristic features of fraud transactions because they are like outliers (occur very rarely) in our dataset, and so the reconstruction error would be higher for them compared to non-fraud transactions. But what do we mean by high reconstruction error? What should be threshold which makes a transaction suspicious?"
 
-    # # Test whether the corpus_viz returns a dictionary
-    # assert isinstance(corpus_viz(corpus1, display=False), dict), "Return type is not a dict"
-    # # Test whether user can access the data frame used to plot the bar chart
-    # assert isinstance(corpus_viz(corpus1, display=False)['df used for bar'], pd.DataFrame), "Return type is not a data frame"
-    # # Test whether user can access the word cloud object
-    # assert isinstance(corpus_viz(corpus2, display=False)['word cloud'], WordCloud), "Return type is not a word cloud"
-    # # Test whether if else statement works - the function should only consider the top 30 most frequently used words
-    # assert corpus_viz(corpus2, display=False)['df used for bar'].shape[0] <= 30, "Too many words"
+    # Test whether the corpus_viz returns a dictionary
+    assert isinstance(corpus_viz(corpus1, display=False), dict), "Return type is not a dict"
+    # Test whether user can access the data frame used to plot the bar chart
+    assert isinstance(corpus_viz(corpus1, display=False)['df used for bar'], pd.DataFrame), "Return type is not a data frame"
+    # Test whether user can access the word cloud object
+    assert isinstance(corpus_viz(corpus2, display=False)['word cloud'], WordCloud), "Return type is not a word cloud"
+    # Test whether if else statement works - the function should only consider the top 30 most frequently used words
+    assert corpus_viz(corpus2, display=False)['df used for bar'].shape[0] <= 30, "Too many words"
 
     
 def test_corpora_compare():
